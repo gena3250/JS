cabf5ba6b30d4be3bf070d5c3716fb696c002c44
gena3250 <69197340+gena3250@users.noreply.github.com>
2023-12-29 12:53:35 +0300

    29122023

--- 10/tools.js
+++ 10/tools.js
@@ -1,3 +1,4 @@
+let colIndex = -1;
 let student = [
 	{
 		name: "Геннадий",
@@ -18,21 +19,21 @@ let student = [
 	{ 	name: "Петр",
 	 	famel: "Петров",
 	 	surname: "Алексеевич",
-	 	date: "01.02.2003",
+	 	date: "01.02.2002",
 	 	year: 2013,
 	 	faculty: "Анформационные системы и техноогии" 
 	},
 	{ 	name: "Василий",
 	 	famel: "Васильев",
 	 	surname: "Хренович",
-	 	date: "01.02.2003",
+	 	date: "01.02.2004",
 	 	year: 2011,
 	 	faculty: "Информационные системы и техноогии" 
 	},
 	{ 	name: "Алексей",
 	 	famel: "Алушкин",
 	 	surname: "Владимирович",
-	 	date: "01.02.2003",
+	 	date: "01.02.2005",
 	 	year: 2012,
 	 	faculty: "Информационные системы и техноогии" 
 	}
@@ -48,17 +49,63 @@ let stud = document.getElementById('stud')
 		this.facultyInput = document.createElement('input');
 		this.buttonInput = document.createElement('button');
 
-		nameInput.placeholder ="Введите имя:";
-		famelInput.placeholder ="Введите фамилию" ;
-		surnameInput.placeholder = "Введите отчество" ;
-		dateInput.type = "date";
+nameInput.placeholder ="Введите имя:";
+famelInput.placeholder ="Введите фамилию" ;
+surnameInput.placeholder = "Введите отчество" ;
+dateInput.type = "date";
 dateInput.value = "1990-01-01";
-		yearInput.placeholder="Год обучения";
-		facultyInput.placeholder = "Факультет";
+yearInput.placeholder="Год обучения";
+facultyInput.placeholder = "Факультет";
 		
-		buttonInput.textContent = "Добавить";
-		buttonInput.classList.add('btn', 'btn-primary')
-		buttonInput.classList.add('mb-3')
+buttonInput.textContent = "Добавить";
+buttonInput.classList.add('btn', 'btn-primary');
+buttonInput.classList.add('mb-3');
+		
+
+//Добавление формы ввода
+
+stud.append(form);
+form.append(nameInput);
+form.append(famelInput);
+form.append(surnameInput);
+form.append(dateInput);
+form.append(yearInput);
+form.append(facultyInput);
+form.append(buttonInput);
+
+//Фильтрация таблицы 
+		this.formFilter = document.createElement('form');
+		this.fioFilter = document.createElement('input');
+		this.facultyFilter = document.createElement('input');
+		this.dateFilter = document.createElement('input');
+		this.yearFilter = document.createElement('input');
+		this.buttonFilter = document.createElement('button');
+	
+formFilter.classList.add('formFilterClass')
+buttonFilter.textContent = "Фильтровать";
+buttonFilter.classList.add('btn', 'btn-primary');
+buttonFilter.classList.add('mb-3');
+		
+fioFilter.placeholder = "ФИО";
+facultyFilter.placeholder = "Факультет";
+dateFilter.placeholder = "ДР";
+yearFilter.placeholder = "Годы обучения";
+		
+
+		stud.append(formFilter);
+		formFilter.append(fioFilter);
+		formFilter.append(facultyFilter);
+		formFilter.append(dateFilter);
+		formFilter.append(yearFilter);
+		formFilter.append(buttonFilter);
+
+		
+
+
+
+
+
+
 
 //Таблица
 
@@ -72,7 +119,7 @@ let tableFacultyTh = document.createElement('th');
 let tableDateTh = document.createElement('th');
 let tableYearTh = document.createElement('th');
 
-
+//Дбавление заголовков таблицы на страницу
 tableFioTh.textContent = "ФИО";
 tableFioTh.setAttribute('data-type','string');
 tableFacultyTh.textContent = "Факультет";
@@ -81,18 +128,10 @@ tableDateTh.textContent = "ДР и возраст";
 tableDateTh.setAttribute('data-type','number');
 tableYearTh.textContent = "Годы обучения";
 tableYearTh.setAttribute('data-type','number');
-
-
-
 table.classList.add("table", "table-dark")
-stud.append(form);
-form.append(nameInput);
-form.append(famelInput);
-form.append(surnameInput);
-form.append(dateInput);
-form.append(yearInput);
-form.append(facultyInput);
-form.append(buttonInput);
+
+
+//Вывод заголовков таблицы
 stud.append(table);
 table.append(thead);
 tableTr.append(tableFioTh);
@@ -105,15 +144,42 @@ thead.append(tableTr);
 
 
 
+
+
+//Функция вывода фильтра
+
+function studFilter(arr,prop,value) {
+	let students = [];
+	copy=[...arr]
+	for (let item of copy) {
+		if (String(item[prop].includes(value) == true)) students.push(item)
+	}
+		return students
+	/* oneUser.birthYear = 2023 - Number(oneUser.date.slice(-4));
+		if (oneUser.birthYear == 20) students.push(oneUser)
+		console.log(students) */	
+}
+let arr1 = studFilter(student, 'name', 'Геннадий');
+console.log(arr1)
+
+/* studFilter(); */
+
+
+
+
+
+
+
+
+
+//Функция вывода таблицы
 function render() {
-	tbody.innerHTML = '';
+tbody.innerHTML = '';
 let students = student.slice();
 
 for (let oneUser of students) {
 	oneUser.fio = oneUser.famel + " " + oneUser.name + " " + oneUser.surname;
-	oneUser.birthYear = 2023 - Number(oneUser.date.slice(-4));
-	console.log(oneUser.birthYear)
-	console.log(typeof oneUser.date)	
+	oneUser.birthYear = 2023 - Number(oneUser.date.slice(-4));//Сколько студенту лет	
 }
 
 
@@ -140,8 +206,10 @@ for (let oneUser of students) {
 
 render();
 
-buttonInput.addEventListener("click", function (e) {
-	e.preventDefault();
+//Добавление в таблицу
+
+buttonInput.addEventListener("click", function (event) {
+	event.preventDefault();
 	let dateNorm = dateInput.value.split("-").reverse().join(".");
 	student.push({
 		name: nameInput.value,
@@ -154,18 +222,20 @@ buttonInput.addEventListener("click", function (e) {
 	render()
 })
 
-
+//Сортировка таблицы по столбцам ,доделать в обратную сторону!!!
 table.addEventListener('click', function(event) {
 	if (event.target.tagName != 'TH') return
-	console.log(event.target);
 	let th = event.target;
-
-	sortTable(th.cellIndex, th.dataset.type);
+	
+	sortTable(th.cellIndex, th.dataset.type, colIndex == th.cellIndex);
+	colIndex = (colIndex == th.cellIndex) ? -1 : th.cellIndex;
 });
  
-function sortTable(colNum, type) {
+
+//Функция сортировки таблицы
+function sortTable(colNum, type,isSorted) {
 	let rowsArrey = Array.from(tbody.rows)
-/* 	console.log(rowsArrey) */
+
 	let compare;
 	switch (type) {
 		case 'number':
@@ -179,7 +249,10 @@ function sortTable(colNum, type) {
 			}
 			break;
 	}
+
 	rowsArrey.sort(compare)
+	if (isSorted) rowsArrey.reverse();
+
 	tbody.append(...rowsArrey)
 }
 
