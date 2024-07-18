674d158d12d02d5d896b78d32c9fff97f5758986
gena3250 <69197340+gena3250@users.noreply.github.com>
2024-07-18 13:44:48 +0300

    18072024

--- 14/app.js
+++ 14/app.js
@@ -1,3 +1,14 @@
+
+
+//создание ссылки
+
+
+
+
+
+
+
+//Запрос
 const request = 'https://gorest.co.in/public/v2/posts';
 async function methodTest(request) {
 	/* const a = 6947824; */
@@ -8,14 +19,16 @@ const response = await fetch(request, {
     'Content-Type':'application/json'
   }
 });
-const blogItem = await response.json();
-	console.log(blogItem[3]);
-	const mass=blogItem.map(element => {
-		return element;
-	});
-
-
-	console.log(mass[1]);
+	const blogItem = await response.json();
+	console.log(blogItem.lenght)
+	for (let i = 0; i < blogItem.length; i++) {
+		let div = document.getElementById('block-library');
+		let link = document.createElement('a');
+		link.setAttribute('href', `http://127.0.0.1:5500/kat.html/${i}`);
+		link.innerText = blogItem[i].title;
+		div.append(link);
+		console.log();
+	}
 	
 	
 	
